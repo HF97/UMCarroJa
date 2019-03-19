652c0e671360636f910b12e2bd8701d496b1f938
HugoFerreira <hugoanf97@gmail.com>
2019-03-19 17:29:48 +0000

    Funcao menuEntrar

--- src/Menu.java
+++ src/Menu.java
@@ -2,25 +2,26 @@ public class Menu {
 
     public static void menuPrincipal(){
         System.out.println(
-                        "1 - Entrar\n" +
-                        "2 - Registar\n\n"+
-                        "0 - Sair"
+               "1 - Entrar\n" +
+               "2 - Registar\n\n"+
+               "0 - Sair"
         );
     }
 
     public static void menuEntrar(){
         System.out.println(
-                        "1 - Entrar Utilizador\n" +
-                        "2 - Entrar Proprietario\n\n" +
-                        "0 - Voltar"
+                "1 - Entrar Utilizador\n" +
+                "2 - Entrar Proprietario\n" +
+                "3 - Entrar Administrador\n\n" +
+                "0 - Voltar"
         );
     }
 
     public  static void menuRegistar(){
         System.out.println(
-                        "1 - Registar utilizador\n" +
-                        "2 - Registar proprietario\n\n" +
-                        "0 - Voltar"
+               "1 - Registar utilizador\n" +
+               "2 - Registar proprietario\n\n" +
+               "0 - Voltar"
         );
     }
 }
--- src/UMCarroJa.java
+++ src/UMCarroJa.java
@@ -40,6 +40,51 @@ public class UMCarroJa {
         }
     }
 
+    public void menuEntrar (Programa p) throws Exception {
+        Scanner input = new Scanner(System.in);
+        int x = -1;
+        limparEcra();
+        Menu.menuEntrar();
+        while(x!=0 && x!=1 && x!=2 && x!=3){
+            x = input.nextInt();
+        }
+        limparEcra();
+        switch(x){
+            case(1):
+                try{
+                    entrarUtilizador(p);
+                }
+                catch (Exception e){
+                    System.out.println("NIF ou Password errado(a).");
+                    entrarUtilizador(p);
+                }
+                break;
+            case(2):
+                try{
+                    entrarProprietario(p);
+                }
+                catch (Exception e){
+                    System.out.println("NIF ou Password errado(a).");
+                    entrarProprietario(p);
+                }
+                break;
+            case(3):
+                try{
+                    entrarAdmin(p);
+                }
+                catch (Exception e){
+                    System.out.println("Password errada.");
+                    entrarAdmin(p);
+                }
+            case(0):
+                limparEcra();
+                executa(p);
+                break;
+        }
+    }
+
+
+
 
     private void limparEcra(){
         System.out.print('\f');
