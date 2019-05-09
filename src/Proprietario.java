05ce142c65471fe46d2947856fe6b3765fa06b17
Hugo Ferreira <hugoanf97@gmail.com>
2019-05-09 15:20:06 +0100

    Historico de carros do proprietario

--- src/Proprietario.java
+++ src/Proprietario.java
@@ -1,34 +1,51 @@
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Proprietario extends Utilizador implements Serializable {
 
     //TODO  Fazer hash com os carros
 
-        private int classificacao;
+    private int classificacao;
+    private List<String> carros;
 
     public Proprietario(){
         super();
         this.classificacao = 0;
+        List<Integer> carros = new ArrayList<Integer>();
     }
 
-    public Proprietario (String nome, int NIF, String email, String password, String morada, LocalDate datanasc, int classificacao, Collection<Integer> histAlugUti){
+    public Proprietario (String nome, int NIF, String email, String password, String morada, LocalDate datanasc, int classificacao, Collection<Integer> histAlugUti, Collection<String> carros){
         super(nome, NIF, email, password, morada, datanasc, histAlugUti);
         this.classificacao = classificacao;
+        this.carros = new ArrayList<String>();
+        for(String s : carros){
+            carros.add(s);
+        }
     }
 
     public Proprietario (Proprietario p){
         super(p);
         this.classificacao = p.getClassificacao();
+        this.carros = p.getCarros();
     }
 
     public int getClassificacao(){
         return this.classificacao;
     }
 
+    public List<String> getCarros() {
+        return carros;
+    }
+
     public void setClassificacao(int c){
         this.classificacao = c;
+    }
+
+    public void setCarros(List<String> carros) {
+        this.carros = carros;
     }
 
     public boolean equals(Object o) {
